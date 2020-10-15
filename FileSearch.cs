38abd096079e5a4bb9f79e048c77bf3385376e9d
Kevyn Herrera <kherrera1738@berkeley.edu>
2020-10-15 11:09:46 -0700

    added file search function

---
+++ FileSearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace PDF_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            string currentDirectory = ".";
+            Queue<string> directories = new Queue<string>();
+            List<string> files = new List<string>();
+            directories.Enqueue(currentDirectory);
+
+            while(directories.Count > 0){
+                currentDirectory = directories.Dequeue();
+                files.AddRange(Directory.EnumerateFiles(currentDirectory, "*.jpg"));
+                foreach(string dir in Directory.EnumerateDirectories(currentDirectory)) {
+                    directories.Enqueue(dir);
+                }                
+
+            }
+
+            foreach (string file in files) {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
